7c4da88c4c9ec1cd19277d260716d238883490d5
bodnard <bodnar1dani@gmail.com>
2022-04-14 12:12:04 +0200

    Base Demo App

---
+++ src/main/java/com/validation/suplychain/demonstration/DemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.validation.suplychain.demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/validation/suplychain/demonstration/DemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validation.suplychain.demonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
